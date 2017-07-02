e09fcba1f5f74b3422d1aec1f6e761a944cfe768
BabyChakra <babychakra@BabyChakras-MacBook-Air.local>
2017-07-02 23:42:54 +0530

    added message in help to change user id and password

--- config/email_tracker.php
+++ config/email_tracker.php
@@ -3,8 +3,8 @@
 $config = [
 	'email_tracker' => [
 		'hostname' => '{imap.gmail.com:993/imap/ssl}INBOX',
-		'username' => 'jd9664557870@gmail.com',
-		'password' => 'AforApple',
+		'username' => 'abcd@gmail.com',
+		'password' => 'passwordGoesHere',
 		'cache_key' => 'last_email_date',
 		'start_date' => '01-Jan-2000'
 	]
